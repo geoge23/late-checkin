40a49a3079a5c8c3603be304e4a208333dc6d723
George Parks <me@georgeparks.me>
2022-09-30 12:56:15 -0400

    kjjj

--- public/js/main.js
+++ public/js/main.js
@@ -1,19 +1,20 @@
-const totp = new jsOTP.totp(30, 8);
-
-let lastGenerationTime = 0;
 const loadBar = document.getElementById('bar');
 const qrHolder = document.getElementById('holder');
+
+otplib.totp.options = { time: 30, digits: 6, encoding: 'ascii' };
+let currentCode;
+
 const interval = setInterval(() => {
     //if last generation time was more than 30s ago, generate a new code
-    if (Date.now() - lastGenerationTime > 30000) {
-        const code = totp.getOtp('MXTFP7AU675MQJQZ');
-        lastGenerationTime = Date.now()
-        console.log(code)
+    if (!otplib.totp.check(currentCode, 'LEMGUTJJNNDAIBJ7')) {
+        const code = otplib.totp.generate('LEMGUTJJNNDAIBJ7');
+        console.log(code, otplib.totp.allOptions())
+        currentCode = code;
         const qr = qrcode(4, 'M');
         qr.addData(`https://localhost:3000/?code=${code}`);
         qr.make();
         qrHolder.innerHTML = qr.createImgTag();
     }
-    loadBar.style.width = `${(1 - (Date.now() - lastGenerationTime) / 30000) * 100}%`
+    loadBar.style.width = `${(otplib.totp.timeRemaining() / 30) * 100}%`
 }, 10);
 
--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var router = express.Router();
 const { authenticator } = require('otplib')
-authenticator.options = { time: 30, digits: 8 }
+authenticator.options = { time: 30, digits: 6, encoding: 'hex', window: 4 }
 
 router.get('/', function(req, res) {
   if (!req.query.code) {
     return res.render('index', { valid: false });
   }
-  console.log(authenticator.check(req.query.code, 'MXTFP7AU675MQJQZ'))
+  console.log(authenticator.generate('LEMGUTJJNNDAIBJ7'), authenticator.allOptions())
+  console.log(req.query.code, authenticator.check(req.query.code, 'LEMGUTJJNNDAIBJ7'))
   return res.render('index', { valid: true });
 });
 
