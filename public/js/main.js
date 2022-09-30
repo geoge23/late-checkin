ec15c7aa3446ff823ff981ed538f41284da1bdcd
George Parks <me@georgeparks.me>
2022-09-30 14:21:29 -0400

    working crypto implementation

--- public/js/main.js
+++ public/js/main.js
@@ -1,20 +1,34 @@
 const loadBar = document.getElementById('bar');
 const qrHolder = document.getElementById('holder');
+const SECRET = 'LEMGUTJJNNDAIBJ7';
 
-otplib.totp.options = { time: 30, digits: 6, encoding: 'ascii' };
-let currentCode;
+const totp = new OTPAuth.TOTP({
+    algorithm: 'SHA1',
+    digits: 6,
+    period: 10,
+    secret: SECRET
+});
+
+let timeStep = 0;
+const periodInMS = totp.period * 1000;
 
 const interval = setInterval(() => {
-    //if last generation time was more than 30s ago, generate a new code
-    if (!otplib.totp.check(currentCode, 'LEMGUTJJNNDAIBJ7')) {
-        const code = otplib.totp.generate('LEMGUTJJNNDAIBJ7');
-        console.log(code, otplib.totp.allOptions())
-        currentCode = code;
-        const qr = qrcode(4, 'M');
-        qr.addData(`https://localhost:3000/?code=${code}`);
-        qr.make();
-        qrHolder.innerHTML = qr.createImgTag();
+    if (timeStep != getTimeStep()) {
+        const code = totp.generate();
+        console.log(code)
+        makeQR(code);
+        timeStep = getTimeStep()
     }
-    loadBar.style.width = `${(otplib.totp.timeRemaining() / 30) * 100}%`
+    loadBar.style.width = `${(1 - (Date.now() % periodInMS) / periodInMS) * 100}%`
 }, 10);
 
+function makeQR(code) {
+    const qr = qrcode(4, 'M');
+    qr.addData(`https://localhost:3000/?code=${code}`);
+    qr.make();
+    qrHolder.innerHTML = qr.createImgTag();
+}
+
+function getTimeStep() {
+    return parseInt(Date.now() / periodInMS);
+}
--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,96 @@
 var express = require('express');
 var router = express.Router();
-const { authenticator } = require('otplib')
-authenticator.options = { time: 30, digits: 6, encoding: 'hex', window: 4 }
+const crypto = require('crypto');
+const { TOTP } = require('otpauth');
+const SECRET = 'LEMGUTJJNNDAIBJ7'
+
+const totp = new TOTP({
+  algorithm: 'SHA1',
+	digits: 6,
+	period: 10,
+	secret: SECRET
+});
 
 router.get('/', function(req, res) {
-  if (!req.query.code) {
+  if (req.query.code && totp.validate({
+    token: req.query.code,
+    window: 0
+  }) != null) {
+    // If the user has a valid code, set a cookie for the day and show
+    // them their late pass
+    res.cookie('pass', generateHmacCookie(), {maxAge: 86400000});
+    return res.render('index', { valid: true, date: generateDateStrings() });
+  } else if (
+    req.cookies.pass && 
+    /[0-9]+:[0-9a-f]+/.test(req.cookies.pass)
+  ) {
+    // If the user has a cookie, check if it's valid for the current day
+    // If so, show their late pass
+    // The regex ensures the cookie matches the HMACCookie schema
+    const [cookieDate, _] = req.cookies.pass.split(':');
+    // validates signature and checks if cookie is from today
+    if (
+      validateHmacCookie(req.cookies.pass) && 
+      isSameDay(
+        new Date(parseInt(cookieDate)), 
+        new Date()
+      )
+    ) {
+      return res.render('index', { valid: true, date: generateDateStrings(new Date(parseInt(cookieDate))) });
+    } else {
+      // If not, delete the cookie and show the error screen
+      res.clearCookie('pass');
+      return res.render('index', { valid: false });
+    }
+  } else {
+    // Otherwise, show them the invalid page
     return res.render('index', { valid: false });
   }
-  console.log(authenticator.generate('LEMGUTJJNNDAIBJ7'), authenticator.allOptions())
-  console.log(req.query.code, authenticator.check(req.query.code, 'LEMGUTJJNNDAIBJ7'))
-  return res.render('index', { valid: true });
 });
 
 module.exports = router;
+
+// Function that checks if dates are on the same day
+// Ensures that the pass was issued on the same actual date as the current date
+// rather than just within a day of checking 
+function isSameDay(date1, date2) {
+  return date1.getFullYear() === date2.getFullYear() &&
+    date1.getMonth() === date2.getMonth() &&
+    date1.getDate() === date2.getDate();
+}
+
+//generate a HMAC cookie with the current date or a supplied date
+function generateHmacCookie(date = new Date()) {
+  const hmac = hmacSha1(date.getTime().toString());
+  return `${date.getTime()}:${hmac}`;
+}
+
+//validate a HMAC cookie with the application secret
+function validateHmacCookie(cookie) {
+  const [date, hmac] = cookie.split(':');
+  return hmac === hmacSha1(date);
+}
+
+//generate a SHA1 hmac string from another string
+function hmacSha1(data) {
+  const hmac = crypto.createHmac('sha1', SECRET);
+  hmac.update(new Buffer.from(data));
+  return hmac.digest('hex');
+}
+
+//generates date string formats for use in the server-side render
+function generateDateStrings(date = new Date()) {
+  console.log(date)
+  const hrs = date.getHours()
+  const mins = date.getMinutes()
+  //adds padding to the minutes (i.e. 1 hrs 2 mins to 1:02 instead of 1:2)
+  const minString = mins.toString().padStart(2, '0')
+  const meridian = hrs < 12 ? 'AM' : 'PM';
+  const timeString = `${hrs % 12}:${minString} ${meridian}`;
+  
+  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+  const monthString = months[date.getMonth()];
+  const day = date.getDate();
+  const dateString = `${monthString} ${day}, ${date.getFullYear()}`
+  return { dateString, timeString };
+}
